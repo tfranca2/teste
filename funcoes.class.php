68e88bcfb752c61d4ef6e100ca2e97d9049920d9
netoFontenele <netinhofontenele@hotmail.com>
2014-03-12 15:57:24 -0200

    Add função de validação de email :D

--- funcoes.class.php
+++ funcoes.class.php
@@ -5,5 +5,12 @@ public class Funcoes(){
 	public dataHora(){
 		return date('Y-m-d H:i');
 	}
+	public function IsValidEmail($email){
+		$conta = "^[a-zA-Z0-9\._-]+@";
+		$domino = "[a-zA-Z0-9\._-]+.";
+		$extensao = "([a-zA-Z]{2,4})$";
+		$pattern = $conta.$domino.$extensao;
+		return (ereg($pattern, $email))?true:false;
+	}
 }
 ?>
